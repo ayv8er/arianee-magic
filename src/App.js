bccbf95253eb5623479ec68f32414b9d47b54d12
Richard Oh <richard@magic.link>
2023-06-21 13:47:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Wallet from "@arianee/wallet";
+import Core from "@arianee/core";
+import { magic } from "./libs/magic";
+import { web3 } from './libs/web3';
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [wallet, setWallet] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkisLoggedIn = async () => {
+      const boolean = await magic.user.isLoggedIn();
+      if (boolean) {
+        setIsLoggedIn(boolean);
+      }
+    };
+    checkisLoggedIn();
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    (async () => {
+      const walletAddress = await web3.eth.getAccounts();
+
+      const core = new Core(
+        async (message) => {
+          const signature = await web3.eth.sign(message, walletAddress[0]);
+          return { message, signature: signature }
+        },
+        async (data) => {
+          const signature = await web3.eth.signTransaction({
+            from: walletAddress[0],
+            to: data.to.toString(),
+            value: data.value.toString(),
+            gas: parseInt(data.gasLimit),
+            maxFeePerGas: parseInt(data.maxFeePerGas),
+            maxPriorityFeePerGas: parseInt(data.maxPriorityFeePerGas),
+            chainId: parseInt(data.chainId),
+            data: data.data,
+          });
+          return { message: data, signature: signature.rawTransaction }
+        },
+        () => {
+          return walletAddress[0];
+        }
+      );
+      const arianee = new Wallet({ chainType: 'testnet', auth: { core: core } })
+      setWallet(arianee)
+    })();
+  }, []);
+
+  const handlePrintMetadata = async () => {
+    try {
+      const metadata = await magic.user.getMetadata();
+      console.log("Metadata", metadata);
+      console.log("arianeeWallet", wallet)
+    } catch (err) {
+      console.log("Not Logged In =>", err);
+    }
+  };
+
+  const handleEmailOTPLogin = async () => {
+    try {
+      await magic.auth.loginWithEmailOTP({ email });
+      setIsLoggedIn(true);
+      console.log("Logged In");
+    } catch (err) {
+      console.log("Login Error =>", err);
+    }
+  };
+
+  const handleLogoutClick = async () => {
+    try {
+      const res = await magic.user.logout();
+      setIsLoggedIn(!res);
+      console.log("Logged Out");
+    } catch (err) {
+      console.log("Logout Error =>", err);
+    }
+  };
+
+  const handleClaim = async () => {
+    try {
+      const res = await wallet.smartAsset.claim('testnet', '74247253', 'ry78jyriuwl7');
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleGetOwned = async () => {
+    try {
+      const res = await wallet.smartAsset.getOwned();
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          disabled={isLoggedIn}
+          placeholder="Enter Email Address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {isLoggedIn ? (
+          <button onClick={handleLogoutClick}>Logout</button>
+        ) : (
+          <button onClick={handleEmailOTPLogin}>Login</button>
+        )}
+      </div>
+      <div>
+        <button onClick={handlePrintMetadata}>Print Info</button>
+        <button onClick={handleClaim}>smartAsset Claim</button>
+        <button onClick={handleGetOwned}>smartAsset GetOwned</button>
+      </div>
+    </main>
   );
 }
 
---
+++ src/libs/magic.js
@@ -0,0 +1,7 @@
+import { Magic } from "magic-sdk";
+
+export const magic = new Magic("pk_live_C86081410ED43355", {
+    network: {
+      rpcUrl: 'https://sokol.arianee.net/'
+    }
+  });
